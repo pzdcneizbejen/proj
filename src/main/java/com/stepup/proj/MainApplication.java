475f3e3e4543051f8b2ade560e703674797ce6ed
pzdcneizbejen <pzdcneizbejen@gmail.com>
2026-01-09 14:11:08 +0300

    Изменена основная логика Добавлен класс коробки

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -3,6 +3,9 @@ package com.stepup.proj;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("hello world!");
-        System.out.println("111111");
+        System.out.println("110011");
+
+        System.out.println("222222");
+        System.out.println("333333");
     }
 }
