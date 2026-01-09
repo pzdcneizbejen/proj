c4d9f53d3e644216647c679827ace45a39983483
pzdcneizbejen <pzdcneizbejen@gmail.com>
2026-01-09 14:15:24 +0300

    Изменена основная логика Изменен метод сделать чего-нибудь

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -4,8 +4,11 @@ public class MainApplication {
     public static void main(String[] args) {
         System.out.println("hello world!");
         System.out.println("110011");
+    }
 
-        System.out.println("222222");
-        System.out.println("333333");
+    public static void doSomething() {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
     }
 }
